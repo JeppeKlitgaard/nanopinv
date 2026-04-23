57ac6caf9a6eb0a4db7b726be1f2b96e87c736b2
Jeppe Klitgaard <jkl@nanonord.dk>
2026-04-23 20:37:31 +0000

    feat: new constructors for MultivariateNormalCholesky

--- nanopinv/distribution.py
+++ nanopinv/distribution.py
@@ -5,9 +5,9 @@ import jax
 import jax.numpy as jnp
 import jaxtyping
 import numpy as np
-from scipy import linalg as la
+from typing import Self
 
-from nanopinv._typing import Array, Float, typecheck
+from nanopinv._typing import Array, Float
 
 
 class DistributionBase(eqx.Module):
@@ -24,16 +24,39 @@ class MultivariateNormalCholesky(DistributionBase):
     shape: tuple[int, ...] = eqx.field(static=True)
     L: Float[Array, "..."]
 
-    # @typecheck
     def __init__(
         self,
+        L: Float[Array, "..."],
+        mean: Float[Array, "..."],
+        shape: tuple[int, ...],
+    ):
+        self.L = L
+        self.mean = mean
+        self.shape = shape
+
+    @classmethod
+    def from_covariance(
+        cls,
         shape: tuple[int, ...],
         mean: Float[Array, "..."] | float,
         cov: Float[Array, "..."],
-    ):
-        self.shape = shape
-        self.L = jax.scipy.linalg.cholesky(cov, lower=True)
-        self.mean = jnp.asarray(mean).ravel()
+    ) -> Self:
+        L = jax.scipy.linalg.cholesky(cov, lower=True)
+        mean = jnp.asarray(mean).ravel()
+
+        return cls(L=L, mean=mean, shape=shape)
+
+
+    @classmethod
+    def from_cholesky(
+        cls,
+        shape: tuple[int, ...],
+        mean: Float[Array, "..."] | float,
+        L: Float[Array, "..."],
+    ) -> Self:
+        mean = jnp.asarray(mean).ravel()
+
+        return cls(L=L, mean=mean, shape=shape)
 
     @eqx.filter_jit
     def __call__(
@@ -57,6 +80,16 @@ class MultivariateNormalCholesky(DistributionBase):
             key,
             shape=rvs_shape,
         )  # (num_samples, r.size)
+
         # Transform to match the covariance structure
         samples = self.mean + z @ self.L.T  # (num_samples, r.size)
         return jnp.reshape(samples, result_shape)
+
+
+def log_likelihood_gaussian(data: Float[Array, "..."], data_obs: Float[Array, "..."], data_std: Float[Array, "..."]) -> Float[Array, ""]:
+    """
+    Compute the log-likelihood of observed data under a Gaussian noise model.
+    """
+    normalised_residual = (data_obs - data) / data_std
+    log_likelihood = -0.5 * jnp.sum(normalised_residual**2)
+    return jnp.sum(log_likelihood)
